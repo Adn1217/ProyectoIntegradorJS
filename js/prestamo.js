b094beceb39e2702e6e624f715adb099a6d0e6e3
Adn1217 <adn1217@hotmail.com>
2022-06-13 23:55:15 -0500

    Simulador modificando el DOM

--- js/prestamo.js
+++ js/prestamo.js
@@ -5,8 +5,8 @@ let monto = 0;
 let cuotas = 0;
 
 do {
-    plazo = parseInt(prompt("Digite número de meses"));
-    if (plazo>0){
+    cuotas = parseInt(prompt("Digite número de meses"));
+    if (cuotas>0){
         valido = true;
     }else {
         continua = confirm("Debe ingresar un número positivo válido")
@@ -32,8 +32,26 @@ if(valido){
         if (monto>0){
             valido = true;
             const prestamo = new Prestamo(tasa, cuotas, monto);
-            let calculo = prestamo.calcularPrestamo(monto, tasa, plazo);
-            console.table(calculo);
+            let calculo = prestamo.calcularPrestamo();
+            console.table(prestamo);
+            const table = document.querySelector("tbody");
+            console.log(table);
+            table.innerHTML = `<tr>
+                <td>${1}</td>
+                <td>${calculo[0][0]}</td>
+                <td>${calculo[1][0]}</td>
+                <td>${calculo[2][0]}</td>
+                <td>${calculo[3][0]}</td>
+                </tr>`;
+            for(let i=1; i<calculo[0].length;i++){
+                table.innerHTML += `<tr>
+                <td>${i+1}</td>
+                <td>${calculo[0][i]}</td>
+                <td>${calculo[1][i]}</td>
+                <td>${calculo[2][i]}</td>
+                <td>${calculo[3][i]}</td>
+                </tr>`
+            }
         }else {
             continua = confirm("Debe ingresar un número positivo válido")
         }
