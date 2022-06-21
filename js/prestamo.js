d71ea3a16e6210ce88951910d2a0d530fd79ecaf
Adn1217 <adn1217@hotmail.com>
2022-06-20 22:00:10 -0500

    Se elimina código comentado

--- js/prestamo.js
+++ js/prestamo.js
@@ -1,90 +1,71 @@
 let valido = false;
 let continua = true;
-// let tasa = 0;
 let monto = 0;
-// let cuotas = 0;
 let calculo = [];
 let cuotaBuscada = 0;
 let valorReferencia = 0;
 
 function Simular (cuotas, tasa, monto) {
-    // do {
-        // cuotas = parseInt(prompt("Digite número de meses"));
-        if (cuotas>0){
-            valido = true;
-            document.getElementById("inputMonths").className="form-control";
-        }else {
-            // continua = confirm("Debe ingresar un número positivo válido.")
-            document.getElementById("inputMonths").className="form-control error";
-            document.getElementById("errorLabel").innerText="Debe ingresar un número positivo válido.";
-        }
-    // }while(!valido && continua)
-    // document.getElementById("inputMonths").value = cuotas;
-
-    if(valido){
-        valido = false;
-        // do {
-            // tasa = parseFloat(prompt("Digite tasa de interés mensual"));
-            if (tasa>0 && tasa<=1){
-                valido = true;
-                document.getElementById("inputRate").className="form-control";
-            }else {
-                // continua = confirm("Debe ingresar un número positivo entre cero y uno.")
-                document.getElementById("inputRate").className="form-control error";
-                document.getElementById("errorLabel").innerText="Debe ingresar un número positivo entre cero y uno.";
-            }
-        // }while(!valido && continua)
-            // document.getElementById("inputRate").value = tasa;
+    if (cuotas>0){
+        valido = true;
+        document.getElementById("inputMonths").className="form-control";
+    }else {
+        document.getElementById("inputMonths").className="form-control error";
+        document.getElementById("errorLabel").innerText="Debe ingresar un número positivo válido.";
     }
 
     if(valido){
         valido = false;
-        // do {
-            // monto = parseFloat(prompt("Digite monto prestado"));
-            if (monto>0){
-                valido = true;
-                const prestamo = new Prestamo(tasa, cuotas, monto);
-                calculo = prestamo.calcularPrestamo();
-                console.table(prestamo);
-                const table = document.querySelector("tbody");
-                // console.log(table);
-                table.innerHTML = `<tr id=cuota1>
-                    <td>${1}</td>
-                    <td>${calculo[0][0]}</td>
-                    <td>${calculo[1][0]}</td>
-                    <td>${calculo[2][0]}</td>
-                    <td class="pagado">${calculo[3][0]}</td>
-                    </tr>`;
-                for(let i=1; i<calculo[0].length;i++){
-                    table.innerHTML += `<tr id=cuota${i+1}>
-                    <td>${i+1}</td>
-                    <td>${calculo[0][i]}</td>
-                    <td>${calculo[1][i]}</td>
-                    <td>${calculo[2][i]}</td>
-                    <td class="pagado">${calculo[3][i]}</td>
-                    </tr>`
-                }
-            document.getElementById("inputAmount").className="form-control";
-            }else {
-                // continua = confirm("Debe ingresar un número positivo válido.")
-                document.getElementById("inputAmount").className="form-control error";
-                document.getElementById("errorLabel").innerText="Debe ingresar un número positivo válido.";
+        if (tasa>0 && tasa<=1){
+            valido = true;
+            document.getElementById("inputRate").className="form-control";
+        }else {
+            document.getElementById("inputRate").className="form-control error";
+            document.getElementById("errorLabel").innerText="Debe ingresar un número positivo entre cero y uno.";
+        }
+    }
+
+    if(valido){
+        valido = false;
+        if (monto>0){
+            valido = true;
+            const prestamo = new Prestamo(tasa, cuotas, monto);
+            calculo = prestamo.calcularPrestamo();
+            console.table(prestamo);
+            const table = document.querySelector("tbody");
+            // console.log(table);
+            table.innerHTML = `<tr id=cuota1>
+                <td>${1}</td>
+                <td>${calculo[0][0]}</td>
+                <td>${calculo[1][0]}</td>
+                <td>${calculo[2][0]}</td>
+                <td class="pagado">${calculo[3][0]}</td>
+                </tr>`;
+            for(let i=1; i<calculo[0].length;i++){
+                table.innerHTML += `<tr id=cuota${i+1}>
+                <td>${i+1}</td>
+                <td>${calculo[0][i]}</td>
+                <td>${calculo[1][i]}</td>
+                <td>${calculo[2][i]}</td>
+                <td class="pagado">${calculo[3][i]}</td>
+                </tr>`
             }
-        // }while(!valido && continua)
-        // document.getElementById("inputAmount").value = monto;
+        document.getElementById("inputAmount").className="form-control";
+        }else {
+            document.getElementById("inputAmount").className="form-control error";
+            document.getElementById("errorLabel").innerText="Debe ingresar un número positivo válido.";
+        }
     }
 
     if(valido && confirm("¿Desea buscar valores de una cuota especifica?")){
-        // do {
-            cuotaBuscada = parseFloat(prompt("Digite la cuota a buscar"));
-            document.getElementById("inputSearchMonth").className="form-control";
-            if(cuotaBuscada<=0){
-                alert("Debe digitar un número mayor a cero")
-                document.getElementById("inputSearchMonth").className="form-control error";
-                document.getElementById("errorLabel").innerText="Debe digitar un número mayor a cero.";
-            }
-        // } while (cuotaBuscada<=0)
-            document.getElementById("inputSearchMonth").value = cuotaBuscada;
+        cuotaBuscada = parseFloat(prompt("Digite la cuota a buscar"));
+        document.getElementById("inputSearchMonth").className="form-control";
+        if(cuotaBuscada<=0){
+            alert("Debe digitar un número mayor a cero")
+            document.getElementById("inputSearchMonth").className="form-control error";
+            document.getElementById("errorLabel").innerText="Debe digitar un número mayor a cero.";
+        }
+        document.getElementById("inputSearchMonth").value = cuotaBuscada;
         if(calculo[4].some((cuota) => cuota === cuotaBuscada)){
             const cuotaEncontrada = document.getElementById(`cuota${cuotaBuscada}`);
             cuotaEncontrada.className = "cuotaEncontrada";
@@ -107,8 +88,6 @@ function Simular (cuotas, tasa, monto) {
         });
 
         alert("Valores de cuota menores en azul.")
-
         document.getElementById("inputSearchAmount").value = valorReferencia;
     }
-    // alert("Muchas gracias por usar nuestros servicios.")
 }
